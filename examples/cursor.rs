2adec24ef4af13bc8715b5c8b3ebbfce802e8c94
4yman <170770027+4yman-0@users.noreply.github.com>
2025-07-16 20:57:16 +0100

    misc: update?

--- examples/cursor.rs
+++ examples/cursor.rs
@@ -6,7 +6,7 @@ use bevy_utils::prelude::*;
 fn main() {
     App::new()
         .add_plugins(RaylibPlugin)
-        .insert_resource(WindowConfig {
+        .insert_resource(RaylibConfig {
             title: "Bevy + Raylib".to_owned(),
             ..default()
         })
@@ -14,11 +14,8 @@ fn main() {
         .run();
 }
 
-fn render(
-    mut raylib: NonSendMut<RaylibHandle>,
-    thread: NonSend<RaylibThreadHandle>,
-) {
-	let cursor = raylib.get_mouse_position();
+fn render(mut raylib: NonSendMut<RaylibHandle>, thread: NonSend<RaylibThreadHandle>) {
+    let cursor = raylib.get_mouse_position();
     let mut d = raylib.begin_drawing(thread.as_ref());
     d.clear_background(Color::WHITE);
     d.draw_text(
--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,35 @@
 //! A Raylib plugin for bevy.
 #![expect(missing_docs, reason = "Docmentation isn't written (yet)")]
+extern crate bevy_app;
+extern crate bevy_ecs;
+extern crate raylib;
+
 use bevy_app::prelude::*;
-use bevy_ecs::prelude::*;
+//use bevy_ecs::prelude::*;
 use raylib::prelude::*;
 
 pub mod prelude {
-    pub use crate::{RaylibConfig, RaylibPlugin, RaylibThreadHandle};
+    pub use crate::{RaylibPlugin, RaylibThreadHandle};
     pub use raylib::prelude::*;
 }
 
-pub struct RaylibPlugin;
+pub struct RaylibPlugin {
+	pub width: i32,
+	pub height: i32,
+	pub title: String,
+	pub vsync: bool,
+}
+
+impl Default for RaylibPlugin {
+    fn default() -> Self {
+        Self {
+            width: 640,
+            height: 480,
+            title: "bevy_raylib2".to_string(),
+            vsync: true,
+        }
+    }
+}
 
 impl Plugin for RaylibPlugin {
     fn build(&self, app: &mut App) {
@@ -17,16 +37,23 @@ impl Plugin for RaylibPlugin {
     }
 }
 
+impl From<&RaylibPlugin> for RaylibBuilder {
+	fn from(from: &RaylibPlugin) -> Self {
+		let mut builder = init();
+		builder
+			.title(from.title.as_str())
+			.height(from.height)
+			.width(from.width);
+		if from.vsync {
+			builder.vsync();
+		}
+		builder
+	}
+}
+
 fn runner(mut app: App) -> AppExit {
-    let builder = app
-        .world_mut()
-        .remove_resource::<RaylibConfig>()
-        .unwrap_or_default();
-    let (rl, thread) = init()
-        .title(builder.title.as_str())
-        .height(builder.height)
-        .width(builder.width)
-        .build();
+    let builder = *app.get_added_plugins::<RaylibPlugin>().first().unwrap();
+    let (rl, thread) = RaylibBuilder::from(builder).build();
     app.world_mut().insert_non_send_resource(rl);
     app.world_mut()
         .insert_non_send_resource(RaylibThreadHandle(thread));
@@ -49,22 +76,5 @@ pub struct RaylibThreadHandle(RaylibThread);
 impl AsRef<RaylibThread> for RaylibThreadHandle {
     fn as_ref(&self) -> &RaylibThread {
         &self.0
-    }
-}
-
-#[derive(Resource)]
-pub struct RaylibConfig {
-    pub width: i32,
-    pub height: i32,
-    pub title: String,
-}
-
-impl Default for RaylibConfig {
-    fn default() -> Self {
-        Self {
-            width: 640,
-            height: 480,
-            title: "bevy_raylib2".to_string(),
-        }
     }
 }

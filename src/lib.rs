22211ca20b7d417c76dc2b9f030252b9780d50bc
4yman-0 <170770027+4yman-0@users.noreply.github.com>
2026-08-02 10:41:24 +0100

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,26 @@
 //! A Raylib plugin for Bevy.
-
+#![no_std]
 //#![add_test("../README.md")]
 
-#![no_std]
-extern crate alloc;
-
-use alloc::string::String;
 use bevy_app::prelude::*;
 //use bevy_ecs::prelude::*;
 use raylib::prelude::*;
 
 /// The `bevy_raylib2` prelude
 pub mod prelude {
-    #[allow(ambiguous_glob_reexports)]
-    pub use crate::*;
-    pub use raylib::prelude::*;
+    pub use crate::{RaylibPlugin};
 }
 
 /// Configuration for the Raylib window and plugin behavior.
 ///
 /// This plugin replaces the Bevy app's runner with a Raylib-powered loop.
 /// All configuration is applied to a `RaylibBuilder` during startup.
-pub struct RaylibPlugin {
-    pub width: i32,
-    pub height: i32,
-    pub title: String,
-    pub vsync: bool,
-}
+pub struct RaylibPlugin<'a>(pub RaylibBuilder<'a>);
 
-impl Default for RaylibPlugin {
-    fn default() -> Self {
-        Self {
-            width: 640,
-            height: 480,
-            title: String::from("bevy_raylib2"),
-            vsync: true,
-        }
-    }
-}
-
-impl Plugin for RaylibPlugin {
+impl Plugin for RaylibPlugin<'static> {
     /// Registers the Raylib runner to override Bevy’s default runner.
     fn build(&self, app: &mut App) {
         app.set_runner(runner);
-    }
-}
-
-impl From<&RaylibPlugin> for RaylibBuilder {
-    /// Converts `RaylibPlugin` configuration into a Raylib window builder.
-    fn from(from: &RaylibPlugin) -> Self {
-        let mut builder = init();
-        builder
-            .title(from.title.as_str())
-            .height(from.height)
-            .width(from.width);
-
-        if from.vsync {
-            builder.vsync();
-        }
-        builder
     }
 }
 
@@ -69,7 +31,7 @@ fn runner(mut app: App) -> AppExit {
         .first()
         .expect("Cannot get Raylib plugin");
 
-    let (rl, thread) = RaylibBuilder::from(builder).build();
+    let (rl, thread) = builder.0.build();
 
     app.world_mut().insert_non_send_resource(rl);
     app.world_mut()
